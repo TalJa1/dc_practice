992de49fdce5a5b1d510dd8bc570052cfa89b4d4
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2025-05-25 12:03:11 +0700

    Add SQL query to calculate total profit by Marital Status and Gender

--- sql_mt/scripts/testScript.sql
+++ sql_mt/scripts/testScript.sql
@@ -1,2 +1,27 @@
 CREATE DATABASE IF NOT EXISTS test_db;
+
 USE test_db;
+
+-- Total profit by MaritialStatus and Gender
+SELECT c.Gender, c.MaritalStatus, SUM(
+        (
+            p.ProductPrice - p.ProductCost
+        ) * s.OrderQuantity
+    ) AS TotalProfit
+FROM
+    tbl_stg_customers c
+    JOIN (
+        SELECT *
+        from tbl_stg_sales_2015
+        UNION
+        SELECT *
+        FROM tbl_stg_sales_2016
+        UNION
+        SELECT *
+        FROM tbl_stg_sales_2017
+    ) s ON c.CustomerKey = s.CustomerKey
+    JOIN tbl_stg_prd p ON s.ProductKey = p.ProductKey
+GROUP BY
+    c.Gender,
+    c.MaritalStatus
+ORDER BY c.Gender, c.MaritalStatus;
