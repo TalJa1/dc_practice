95bb81bcd099867f7a2e060aed0936a384df39d8
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2025-05-26 09:29:01 +0700

    Refactor SQL queries for clarity and consistency in formatting

--- sql_mt/scripts/live_sql.sql
+++ sql_mt/scripts/live_sql.sql
@@ -32,28 +32,46 @@ SELECT pc.`CategoryName`, SUM(
             p.ProductPrice - p.ProductCost
         ) * s.OrderQuantity
     ) AS TotalProfit
-from tbl_stg_prdcat pc
-JOIN tbl_stg_prdsubcat pr
-USING (`ProductCategoryKey`)
-JOIN tbl_stg_prd p
-USING (`ProductSubcategoryKey`)
-JOIN (
-    SELECT *
-    FROM tbl_stg_sales_2015
-    UNION
-    SELECT *
-    FROM tbl_stg_sales_2016
-    UNION
-    SELECT *
-    FROM tbl_stg_sales_2017
-) s
-USING (`ProductKey`)
-GROUP BY pc.`CategoryName`
+from
+    tbl_stg_prdcat pc
+    JOIN tbl_stg_prdsubcat pr USING (`ProductCategoryKey`)
+    JOIN tbl_stg_prd p USING (`ProductSubcategoryKey`)
+    JOIN (
+        SELECT *
+        FROM tbl_stg_sales_2015
+        UNION
+        SELECT *
+        FROM tbl_stg_sales_2016
+        UNION
+        SELECT *
+        FROM tbl_stg_sales_2017
+    ) s USING (`ProductKey`)
+GROUP BY
+    pc.`CategoryName`
 
 -- How many customers are there for each MaritalStatus and Gender combination? List MaritalStatus, Gender, and the count of customers
 SELECT c.`MaritalStatus`, c.`Gender`, COUNT(*)
 FROM tbl_stg_customers c
-GROUP BY c.`MaritalStatus`, c.`Gender`
+GROUP BY
+    c.`MaritalStatus`,
+    c.`Gender`
 
 -- What is the total OrderQuantity sold for each product? List the ProductName and the sum of OrderQuantity.
 -- Order the results by total quantity sold in descending order
+CREATE View sales AS
+SELECT *
+FROM tbl_stg_sales_2015
+UNION ALL
+SELECT *
+FROM tbl_stg_sales_2016
+UNION ALL
+SELECT *
+FROM tbl_stg_sales_2017;
+
+SELECT p.`ProductName`, SUM(s.`OrderQuantity`) AS TotalOrderQuantity
+FROM tbl_stg_prd p
+    JOIN sales s USING (`ProductKey`)
+GROUP BY
+    p.`ProductName`
+ORDER BY TotalOrderQuantity DESC;
+
