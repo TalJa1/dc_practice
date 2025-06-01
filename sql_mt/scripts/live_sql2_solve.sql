81872fe065e1f3fbc3b849c457cf4ed3feda0efb
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2025-06-01 13:44:33 +0700

    Add SQL query to identify underperforming agents based on order count

--- sql_mt/scripts/live_sql2_solve.sql
+++ sql_mt/scripts/live_sql2_solve.sql
@@ -1,2 +1,9 @@
 use sqlbusinessproblem;
 
+-- Problem 1: Identify 5 Underperforming Agents
+SELECT a.`AGENT_NAME`, COUNT(o.`ORD_AMOUNT`) AS `TOTAL_ORDERS`, RANK() OVER (ORDER BY COUNT(o.`ORD_AMOUNT`) ASC) AS `RANK`
+FROM agents a
+JOIN orders o
+USING(`AGENT_CODE`)
+GROUP BY a.`AGENT_NAME`
+HAVING COUNT(o.`ORD_AMOUNT`) < 5
