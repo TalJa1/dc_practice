e5e9456d71de16bd26b8301cf4c7bdd902b7d806
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2025-06-01 13:50:36 +0700

    Format SQL query for better readability in underperforming agents identification

--- sql_mt/scripts/live_sql2_solve.sql
+++ sql_mt/scripts/live_sql2_solve.sql
@@ -1,9 +1,12 @@
 use sqlbusinessproblem;
 
 -- Problem 1: Identify 5 Underperforming Agents
-SELECT a.`AGENT_NAME`, COUNT(o.`ORD_AMOUNT`) AS `TOTAL_ORDERS`, RANK() OVER (ORDER BY COUNT(o.`ORD_AMOUNT`) ASC) AS `RANK`
+SELECT a.`AGENT_NAME`, COUNT(o.`ORD_AMOUNT`) AS `TOTAL_ORDERS`, RANK() OVER (
+        ORDER BY COUNT(o.`ORD_AMOUNT`) ASC
+    ) AS `RANK`
 FROM agents a
-JOIN orders o
-USING(`AGENT_CODE`)
-GROUP BY a.`AGENT_NAME`
-HAVING COUNT(o.`ORD_AMOUNT`) < 5
+    JOIN orders o USING (`AGENT_CODE`)
+GROUP BY
+    a.`AGENT_NAME`
+HAVING
+    COUNT(o.`ORD_AMOUNT`) < 5
